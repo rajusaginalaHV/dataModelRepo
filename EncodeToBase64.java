4c7cb12f9144a42e4cc8b8d31d572d38785bfd76
rajusaginalaHV <137863686+rajusaginalaHV@users.noreply.github.com>
2023-07-05 20:54:17 +0530

    Update EncodeToBase64.java

--- EncodeToBase64.java
+++ EncodeToBase64.java
@@ -16,7 +16,6 @@ public static void main(String[] args){
 
     String str = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:v2=\"http://xmlns.oracle.com/oxp/service/v2\">\r\n   <soapenv:Header/>\r\n   <soapenv:Body>\r\n      <v2:uploadObject>\r\n         <v2:reportObjectAbsolutePathURL>"+args[2]+"</v2:reportObjectAbsolutePathURL>\r\n         <v2:objectType>xdmz</v2:objectType>\r\n         <v2:objectZippedData>"+encoder.encodeToString(array)+"</v2:objectZippedData>\r\n         <v2:userID>"+args[3]+"</v2:userID>\r\n         <v2:password>"+args[4]+"</v2:password>\r\n      </v2:uploadObject>\r\n   </soapenv:Body>\r\n</soapenv:Envelope>";
     BufferedWriter writer = new BufferedWriter(new FileWriter(args[1]));
-    System.out.println(str);
     writer.write(str);
      writer.close();
     }catch(Exception e){
